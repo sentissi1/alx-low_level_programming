a70e718d89717c1329b6b9bdd10beb804f4a37db
sentissi1 <sentissielmehdi@gmail.com>
2023-09-12 12:52:59 -0700

    hero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <unistd.h>
 #include "main.h"
 /**
  * main - print _putchar
@@ -6,8 +5,17 @@
  * Return: 0
  */
 
-int _putchr(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
 
